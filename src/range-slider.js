a657c46fc923e1fddac0a76d4f1b2e9b75673d32
benjamin.brodwolf <benjamin.brodwolf@students.fhnw.ch>
2020-01-08 20:17:06 +0100

    desing filterzone

--- src/range-slider.js
+++ src/range-slider.js
@@ -95,7 +95,7 @@ const setInputfieldValue = (input, min, max) => {
     setTooltipPosition(masterSliderNode, sndTooltip, high, "--second");
 };
 
-const setDomSlider = (max, min, valuetype, id) => `<div  class='range-slider' propID='${id}' valuetype='${valuetype}' min='${min}' max='${max}' style="--width: 180px; --low:0%; --high:100%">
+const setDomSlider = (max, min, valuetype, id) => `<div  class='range-slider' propID='${id}' valuetype='${valuetype}' min='${min}' max='${max}' style="--width: 190px; --low:0%; --high:100%">
                                                         <div class="range-bg"></div>
                                                         <span class="fst-value">${min}</span>
                                                         <input type='range' step="0.1" value='0' level="low" oninput='setSliderValue(this)' />
